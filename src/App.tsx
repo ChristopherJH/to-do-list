87e5a366a724031195254335adb9b917dcb33ba2
Christopher Hallett <chrisjameshal@gmail.com>
2021-11-17 18:07:26 +0000

    Displayed to-do items from express server

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,74 @@
-import { greet } from "./utils/greet";
+import React, { useEffect, useState } from "react";
+
+interface ToDoProps {
+  id: number;
+  task: string;
+  creationDate: string;
+  dueDate: string;
+}
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  const [toDoItems, setToDoItems] = useState<ToDoProps[]>([])
+
+  useEffect(() => {
+    const fetchLink = async () => {
+      const response = await fetch("http://localhost:4000/items");
+      const data = await response.json();
+      setToDoItems(data);
+    };
+    fetchLink();
+  }, []);
+  console.log(toDoItems)
+  return (
+    <div className="body">
+      <Header />
+      <ToDoList
+        toDoItems={toDoItems}
+      />
+    </div>
+  );
+}
+
+function Header(): JSX.Element {
+  return (
+    <>
+      <h1>My To-do List</h1>
+    </>
+  );
+}
+
+interface ToDoItemCardProps {
+  toDoItem: ToDoProps;
+}
+
+function ToDoItemCard(props: ToDoItemCardProps): JSX.Element {
+  return (
+    <div className="todo-item">
+      <button>Complete</button>
+      <span>{props.toDoItem.task}</span>
+      <span>{props.toDoItem.creationDate}</span>
+      <span>{props.toDoItem.dueDate}</span>
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  )
+}
+
+interface ToDoListProps {
+  toDoItems: ToDoProps[];
+}
+
+function ToDoList(props: ToDoListProps): JSX.Element {
+  return (
+    <div className="todo-list">
+      {props.toDoItems.map((item) => 
+        <ToDoItemCard
+          toDoItem={item}
+          key={item.id}
+        />
+      )};
+    </div>
+  )
 }
 
 export default App;
