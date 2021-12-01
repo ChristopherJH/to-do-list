8018c18c59637383b31b9ab8c9294e8e2bce018d
Christopher Hallett <chrisjameshal@gmail.com>
2021-12-01 08:52:53 +0000

    Add complete task functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 interface ToDoProps {
   id: number;
   task: string;
   creationDate: string;
   dueDate: string;
+  completed: boolean;
 }
+
+const baseURL = "http://localhost:4000/"
 
 function App(): JSX.Element {
   const [toDoItems, setToDoItems] = useState<ToDoProps[]>([]);
 
   useEffect(() => {
     const fetchLink = async () => {
-      const response = await fetch("http://localhost:4000/items");
+      const response = await fetch(baseURL + "items");
       const data = await response.json();
       setToDoItems(data);
     };
@@ -22,7 +25,7 @@ function App(): JSX.Element {
   return (
     <>
       <Header />
-      <ToDoList toDoItems={toDoItems} />
+      <ToDoList toDoItems={toDoItems} setToDoItems={setToDoItems}/>
     </>
   );
 }
@@ -37,30 +40,41 @@ function Header(): JSX.Element {
 
 interface ToDoItemCardProps {
   toDoItem: ToDoProps;
+  setToDoItems: (input: ToDoProps[]) => void;
 }
 
 function ToDoItemCard(props: ToDoItemCardProps): JSX.Element {
+  const itemProps = props.toDoItem
   return (
     <div className="todo-item">
-      <button className="todo-complete"></button>
-      <p>{props.toDoItem.task}</p>
-      <p>{props.toDoItem.creationDate}</p>
-      <p>{props.toDoItem.dueDate}</p>
+      {!itemProps.completed && <button className="todo-complete" onClick={() => handleComplete(itemProps.id, props.setToDoItems)}></button>}
+      <p>{itemProps.task}</p>
+      <p>{itemProps.creationDate}</p>
+      <p>{itemProps.dueDate}</p>
       <button className="todo-edit">Edit</button>
       <button className="todo-delete">X</button>
     </div>
   );
 }
 
+async function handleComplete(id: number, setToDoItems: (input: ToDoProps[]) => void) {
+  console.log('Complete button pressed')
+  const response = await fetch(baseURL + "items/" + id.toString() + "/complete", {method: 'PUT'});
+  const data = await response.json();
+  setToDoItems(data);
+  console.log('Complete button processed')
+}
+
 interface ToDoListProps {
   toDoItems: ToDoProps[];
+  setToDoItems: (input: ToDoProps[]) => void;
 }
 
 function ToDoList(props: ToDoListProps): JSX.Element {
   return (
     <div className="todo-list">
       {props.toDoItems.map((item) => (
-        <ToDoItemCard toDoItem={item} key={item.id} />
+        <ToDoItemCard toDoItem={item} key={item.id} setToDoItems={props.setToDoItems}/>
       ))}
     </div>
   );
